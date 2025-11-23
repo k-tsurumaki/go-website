84d07b46ecdc5cae3888a1b0aa6263a1ab862361
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-11-23 11:40:32 +0900

    add templates

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Printf("Server is running on :%s\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
-	
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
